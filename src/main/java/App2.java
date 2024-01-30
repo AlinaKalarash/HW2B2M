f796074943b8a7e8a881d45183a4f0008a7f0860
Alina Kalarash <bloha7895@gmail.com>
2024-01-31 01:51:33 +0200

    safe commit3.0

--- src/main/java/App2.java
+++ src/main/java/App2.java
@@ -15,9 +15,7 @@ public class App2 {
     static byte rand;
     static byte i;
     static boolean boxAvailable;
-    static byte winner = 0;
     static char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
 
     static boolean boxEmpty = false;
 
@@ -36,21 +34,9 @@ public class App2 {
                     box[i] = ' ';
                 boxEmpty = true;
             }
+            sc();
 
 
-
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                } else
-                    System.out.println("Invalid input. Enter again.");
-            }
 
             boxAvailable = false;
             for (i = 0; i < 9; i++) {
@@ -61,35 +47,70 @@ public class App2 {
             }
 
             if (!boxAvailable) {
-
                 resultOfGame();
                 break;
-
             }
 
-            while (true) {
-                rand = (byte) (Math.random() * 9 + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
+            randNum();
+
+        }
+    }
+
+    public static void sc() {
+        while (true) {
+            input = scan.nextByte();
+            if (input > 0 && input < 10) {
+                if (box[input - 1] == 'X' || box[input - 1] == 'O')
+                    System.out.println("That one is already in use. Enter another.");
+                else {
+                    box[input - 1] = 'X';
                     break;
                 }
+            } else
+                System.out.println("Invalid input. Enter again.");
+        }
+    }
+
+    public static void randNum() {
+        while (true) {
+            rand = (byte) (Math.random() * 9 + 1);
+            if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                box[rand - 1] = 'O';
+                break;
             }
         }
     }
 
     public static void resultOfGame() {
-        if ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X') || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X') ||
-                (box[0] == 'X' && box[3] == 'X' && box[6] == 'X') || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X') || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X') ||
-                (box[0] == 'X' && box[4] == 'X' && box[8] == 'X') || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X')) {
+        if (isItWin('X')) {
             System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-        } else if ((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O') || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O') ||
-                (box[0] == 'O' && box[3] == 'O' && box[6] == 'O') || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O') || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O') ||
-                (box[0] == 'O' && box[4] == 'O' && box[8] == 'O') || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O')) {
+
+        } else if (isItWin('O')) {
             System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+
         } else {
             System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
 
         }
 
+    }
+
+    public static boolean isItWin(char a) {
+        boolean win = false;
+        for (int i = 0; i < 3; i++) {
+
+//            Перевірка горизонтальних та вертикальних ліній
+            if ( (box[i * 3] == a && box[i * 3 + 1] == a && box[i * 3 + 2] == a) || /*Перевірка горизонтальних ліній*/
+            (box[i] == a && box[i + 3] == a && box[i + 6] == a)){ /*Перевірка вертикальних ліній*/
+                win = true;
+                break;
+            }
+        }
+//            Перевірка діагоналей
+        if ((box[0] == a && box[4] == a && box[8] == a) || (box[2] == a && box[4] == 'X' && box[6] == a)) {
+            win = true;
+        }
+
+        return win;
     }
 }
