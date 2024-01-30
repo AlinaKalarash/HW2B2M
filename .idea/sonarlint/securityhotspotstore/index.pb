e5e87d62d90aad6d4bf908ed7f8b314a821bb71f
Alina Kalarash <bloha7895@gmail.com>
2024-01-30 19:04:51 +0200

    safe commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+F
+src/main/java/App.java,6\7\67512e2f04487cca15d6699055f74a51de1a9eb4
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+7
+App.iml,0\5\05fdf6b79c015c911b10f69ff83ed7ce9a822a74
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
+w
+G.idea/sonarlint/issuestore/0/5/05fdf6b79c015c911b10f69ff83ed7ce9a822a74,8\f\8fcc5ee5fe44d909cd603c51b9f90d83f2a7caaf
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2\c\2cabb939da04d3de3a200bf2b575690e1a16316d
+Å
+Q.idea/sonarlint/securityhotspotstore/0/5/05fdf6b79c015c911b10f69ff83ed7ce9a822a74,6\d\6d174b00274f95675f7cc19c0b09632d6f1323a0
+Å
+Q.idea/sonarlint/securityhotspotstore/6/7/67512e2f04487cca15d6699055f74a51de1a9eb4,6\d\6d745d781a776adac3fa77c8278111a3dff5a966
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
         byte i;
         boolean boxAvailable = false;
         byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         System.out.println("Enter box number to select. Enjoy!\n");
 
         boolean boxEmpty = false;
@@ -19,20 +19,20 @@ public class App {
             System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
             System.out.println("-----------");
             System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if(!boxEmpty){
-                for(i = 0; i < 9; i++)
+            if (!boxEmpty) {
+                for (i = 0; i < 9; i++)
                     box[i] = ' ';
                 boxEmpty = true;
             }
 
-            if(winner == 1){
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 2){
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 3){
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            if (winner > 0) {
+                if (winner == 1) {
+                    System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+                } else if (winner == 2) {
+                    System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+                } else {
+                    System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+                }
                 break;
             }
 
@@ -45,46 +45,45 @@ public class App {
                         box[input - 1] = 'X';
                         break;
                     }
-                }
-                else
+                } else
                     System.out.println("Invalid input. Enter again.");
             }
 
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                   winner = 1;
-                   continue;
-            }
-
             boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
+            for (i = 0; i < 9; i++) {
+                if (box[i] != 'X' && box[i] != 'O') {
                     boxAvailable = true;
                     break;
                 }
             }
 
-            if(boxAvailable == false){
-                winner = 3;
-                continue;
-            }
+            if (!boxAvailable) {
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
+                if ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X') || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X') ||
+                        (box[0] == 'X' && box[3] == 'X' && box[6] == 'X') || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X') || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X') ||
+                        (box[0] == 'X' && box[4] == 'X' && box[8] == 'X') || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X')) {
+                    winner = 1;
+                } else if ((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O') || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O') ||
+                        (box[0] == 'O' && box[3] == 'O' && box[6] == 'O') || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O') || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O') ||
+                        (box[0] == 'O' && box[4] == 'O' && box[8] == 'O') || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O')) {
+                    winner = 2;
+                } else {
+                    winner = 3;
                 }
+                    continue;
+
             }
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
-                winner = 2;
-                continue;
+
+                while (true) {
+                    rand = (byte) (Math.random() * 9 + 1);
+                    if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                        box[rand - 1] = 'O';
+                        break;
+                    }
+                }
+
             }
+
         }
-
     }
-}
